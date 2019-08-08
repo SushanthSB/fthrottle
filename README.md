e38f64696dc792f9b5f98e6a3d18b024cc13258c
Sushanth SB <sushanth@versionn.com>
2019-08-08 18:22:46 +0530

    updated readme.md, added missing in views.py

--- README.md
+++ README.md
@@ -14,6 +14,18 @@ Inside server folder:
 * pip3 install -r requriements.txt to install all the dependencies
 * python3 manage.py migrate to get models into database
 * Injest word_search.tsv to app_data table.
+	* to injest data from tsv:
+		1. Enter into mysql CLI and execute below query.
+		1. load data local infile {path/to/word_search.tsv} into table app_data
+			 fields terminated by '\t'
+			 enclosed by '"'
+			 lines terminated by '\n'
+			 (string, occurance)
+* <b>python3 manage.py test {path/for/tests_folder}</b> to get test results.
 
 > * npm run build will build static files and also copy them to static folder in server directory so that django can serve the static files.
 > * While running client and server under different origin during development browser's CORS extension will be required.
+
+# Info
+* Django backend exposes <b>/search</b> api for the client, it takes query parameter <b>word</b> ex: /search?word={pattern}
+* Based on given pattern it returns json containing array of suggestions upto 25 strings
--- server/app/views.py
+++ server/app/views.py
@@ -29,7 +29,7 @@ def match(pat, txt):
 
 def insert_position(index, array):
     ''' Takes index of pattern occurance in text and array
-        of pattern occurance indexes, finds where the new index
+        of pattern positions in text, finds where the new index
         has to be inserted and returns the index '''
 
     for key, val in enumerate(array):
@@ -40,26 +40,39 @@ def insert_position(index, array):
 
 @require_GET
 def search(request):
+    ''' Takes GET request with "word" parameter and returns json 
+        response containing array of matched words from database '''
+
     pattern = []
     occurance_idx = []
 
     word = request.GET.get("word", "")
     if word != "":
+        # Get strings that contains the given pattern
         dataset = Data.objects.filter(
             string__contains=word).order_by('-occurance').values_list(
             'string', flat=True)
 
+        # loop through data set and arrange them based on
+        # order of occurance, length of the string and number of occurances
         for val in dataset:
             text = match(word, val)
             if text is not None:
                 if text[0] == word:
+                    # if the pattern and string is a perfect match
+                    # insert at beginning of the list
                     pattern.insert(0, text[0])
                     occurance_idx.insert(0, 0)
                 else:
                     if len(occurance_idx) == 0:
+                        # if the pattern list and ocurance_idx list is empty
+                        # append the text and position
                         occurance_idx.append(text[1])
                         pattern.append(text[0])
                     else:
+                        # if the list is not empty search for postion
+                        # in occurance_idx list and insert the text at 
+                        # appropriate index. 
                         index = insert_position(text[1], occurance_idx)
                         occurance_idx.insert(index, text[1])
                         pattern.insert(index, text[0])
@@ -70,6 +83,7 @@ def search(request):
     return JsonResponse(
         {
             "success": True,
+            # sort the pattern_list based on length of strings in pattern list
             "data": sorted(pattern, key=len)
         }
     )
